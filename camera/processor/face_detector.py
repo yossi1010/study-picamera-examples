bb1140c4930a0dbe9f28321e671254f81e261425
yossi1010 <41571555+yossi1010@users.noreply.github.com>
2018-07-23 04:58:01 -0700

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -39,7 +39,8 @@ class FaceDetector(object):
         # 取得した座標情報を元に、cv2.rectangleを使ってframe上に
         # 顔の位置を描画する
         count = 0
-        putText(count)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame,'count,(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
             count += 1
